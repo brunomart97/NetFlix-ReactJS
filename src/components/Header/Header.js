8b48f4b9cf124031a552262e1293f19d5351dcf7
Bruno Martins <67600534+brunomart97@users.noreply.github.com>
2021-01-11 18:36:37 -0300

    NetFlix Clone

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+    return(
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-2-1.png" alt="Logo Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Usuário" />
+                </a>
+            </div>
+        </header>
+    );
+}
